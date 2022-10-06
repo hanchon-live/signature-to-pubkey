4989488c2bab967d9ef466e04645b4419192a9e1
hanchon <guillermo.paoletti@gmail.com>
2022-10-06 22:40:44 +0200

    chore: build the lib

--- lib/converter.d.ts
+++ lib/converter.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="node" />
+export declare const recoverPubKey: (msgHash: Buffer, v: bigint, r: Buffer, s: Buffer, chainId?: bigint | undefined) => string;
 export declare function signatureToPubkey(signature: string, msgHash: Buffer): string;
 export declare function fromHexString(hexString: string): Uint8Array;
